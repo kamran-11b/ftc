a9959a5561ca13c42f09494d69d09a12fa176b49
kamran-11b <j.kamran14@gmail.com>
2023-04-09 17:52:59 +0600

    New Update

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ const swiper3 = new Swiper('.swiper3', {
     // Optional parameters
     loop: true,
     autoplay: {
-        delay: 3000,
+        delay: 3500,
     },
 
 });
@@ -53,12 +53,51 @@ const swiper4 = new Swiper('.swiper4', {
     loop: true,
 
 });
+const swiper5 = new Swiper('.swiper5', {
+    // Optional parameters
+    loop: true,
 
-new Splide('.splide', {
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    autoplay: {
+        delay: 3000,
+    },
+});
+
+
+new Splide('#splide-1', {
+    type: 'loop',
+    autoplay: true,
+    perPage: 3,
+    gap: "2em",
+    focus: 0,
+    perPage: 4,
+    breakpoints: {
+        1024: {
+            perPage: 3,
+
+        },
+        767: {
+            perPage: 2,
+
+        },
+        640: {
+            perPage: 1,
+
+        },
+    },
+
+}).mount();
+
+
+new Splide('#splide-2', {
     type: string = 'fade',
     perMove: 1,
     drag: 'free',
     autoplay: 'pause',
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    gap: "2em",
 }).mount();
